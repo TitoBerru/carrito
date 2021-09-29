0787746f23029fe97e45b9dd6f4a6f7cab71bea8
Tito Berru <c_myco@hotmail.com>
2021-09-29 00:22:56 -0300

    Subo Apis

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override');
 
 const logMiddleware = require('./middlewares/logMiddlewares');
 const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
+const cors = require('cors');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(session({
     saveUninitialized: false,
 }));
 app.use(userLoggedMiddleware);
+app.use(cors());
 
 
 app.use(express.static(path.join(__dirname, './public')));
@@ -36,16 +38,34 @@ app.set('views', path.join(__dirname, '/views'));
 const mainRoutes = require('./src/routes/mainRoutes');
 const productRoutes = require('./src/routes/productRoutes');
 const userRoutes = require('./src/routes/userRoutes');
+
+
 //**Rutas de APiS */
 const ApiSeleccionRoutes = require ('./src/routes/ApiSeleccionRoutes');
+const APIservicioRoutes = require ('./src/routes/APIservicioAdicionalRoutes');
+const APIusuarioRoutes = require ('./src/routes/APIusuarioRoutes');
+const APIusuarioProductoRoutes = require ('./src/routes/APIusuarioProductoRoutes');
+const APIpackRoutes = require ('./src/routes/APIpackRoutes')
+
+
 
 
 app.use('/', mainRoutes);
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
 app.use('/apis', ApiSeleccionRoutes);
+app.use('/seleccion',ApiSeleccionRoutes);
+app.use('/servicio',APIservicioRoutes);
+app.use('/busquedaUsuario', APIusuarioRoutes);
+app.use('/busquedaVenta', APIusuarioProductoRoutes);
+app.use('/busquedaProducto', APIpackRoutes);
 
-app.listen(process.env.PORT || 3000, function () {
+
+
+
+
+
+app.listen(process.env.PORT || 3001, function () {
     console.log('Servidor corriendo en el puerto 3000');
 });
 

02bb6af783ffbec10b71af2b0349ce5840e4967b
Tito Berru <c_myco@hotmail.com>
2021-10-28 17:31:17 -0300

    sigo probando rutas de apis

--- src/routes/apiSelectRoutes.js
+++ src/routes/apiSelectRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const apiSelect = require('../apis/apiSelect');
 // const ApiConfirmaCompra = require('../Apis/ApiConfirmaCompra');
 

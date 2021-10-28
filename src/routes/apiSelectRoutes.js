653c1d195d51091873133fdd0459d20987aec6fd
Tito Berru <c_myco@hotmail.com>
2021-10-28 17:38:18 -0300

    Sigo probando, lo anterior funciono. A heroku parece q no le gusta la carpeta APIS

--- src/routes/apiSelectRoutes.js
+++ src/routes/apiSelectRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const apiSelect = require('../controllers/apiSelect');
-// const ApiConfirmaCompra = require('../Apis/ApiConfirmaCompra');
+const apiSelect = require('../apisControllers/apiSelect');
+
 
 router.get('/', apiSelect.lista);
 router.get('/:id', apiSelect.uno);
-// router.post('/compra', ApiConfirmaCompra.compra)
+
 
 module.exports = router;
